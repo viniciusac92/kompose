f3ffe87356a8d79e9bce618e0e0b4393e01500d4
ViniciusAC29 <vinicius.campello@hotmail.com>
2021-08-31 15:22:51 -0300

    feat: :wrench: add config files for heroku deploy

--- kompose/settings.py
+++ kompose/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
+import dj_database_url
 import environ
 
 env = environ.Env()
@@ -102,6 +103,13 @@ else:
         }
     }
 
+DATABASE_URL = env("DATABASE_URL")
+
+if DATABASE_URL:
+    config = dj_database_url.config(
+        default=DATABASE_URL, conn_max_age=500, ssl_require=True
+    )
+    DATABASES["default"] = config
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
